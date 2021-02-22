4165a25d3abd93ea851c136d1430b3279fe0dc0a
Daniel Guest <guestusut@gmail.com>
2021-02-22 15:42:27 -0600

    Added TEN noise synthesis code

--- apcmodels/signal.py
+++ apcmodels/signal.py
@@ -1,5 +1,8 @@
 import numpy as np
-from math import floor, ceil
+from math import floor
+import math
+from scipy.interpolate import interp1d
+from copy import deepcopy
 
 
 def amplify(signal, dB):
@@ -156,3 +159,101 @@ def scale_dbspl(signal, dB):
     curr_dB = dbspl_pascal(signal)
     delta_dB = dB - curr_dB
     return amplify(signal, delta_dB)
+
+
+def te_noise(duration, fs, lco, hco, level):
+    """
+    Synthesizes a sample of threshold-equalizing noise. Adapted with minor changes from code used internally in the
+    Auditory Perception and Cognition Lab at the University of Minnesota.
+
+    Arguments:
+        duration (float): duration in seconds
+        fs (int): sampling rate in Hz
+        lco (float): lower cutoff of the noise in Hz
+        hco (float): higher cutoff of the noise in Hz
+        level (float): level of the noise in the ERB centered on 1 kHz
+
+    Returns:
+        noise (ndarray): array of noise of shape (n_sample, )
+    """
+    # Raise errors if anything is fishy
+    if lco < 0:
+        raise ValueError('lco should not be below 0 Hz')
+    if hco > fs/2:
+        raise ValueError('hco should be below Nyquist frequency')
+
+    # Synthesize noise
+    dur_smp = floor(duration * fs)
+
+    fftpts = dur_smp
+
+    binfactor = fftpts/fs
+    LPbin = round(lco*binfactor)
+    HPbin = round(hco*binfactor)
+
+    a = np.zeros((fftpts))
+    b = np.zeros((fftpts))
+
+    a[LPbin:HPbin] = np.random.randn(HPbin-LPbin)
+    b[LPbin:HPbin] = np.random.randn(HPbin-LPbin)
+
+    fspec = a + b*1j
+
+    local_freq_bin = np.arange(LPbin, HPbin)
+    frequency = ((local_freq_bin)/binfactor)/1000
+    all_freqs = (np.arange(0, fftpts)/binfactor)/1000
+
+    # From personal correspondence with B. C. J. Moore via A. J. Oxenham
+    K = np.array([[0.0500, 13.5000],
+                  [0.0630, 10.0000],
+                  [0.0800, 7.2000],
+                  [0.1000, 4.9000],
+                  [0.1250, 3.1000],
+                  [0.1600, 1.6000],
+                  [0.2000, 0.4000],
+                  [0.2500, -0.4000],
+                  [0.3150, -1.2000],
+                  [0.4000, -1.8500],
+                  [0.5000, -2.4000],
+                  [0.6300, -2.7000],
+                  [0.7500, -2.8500],
+                  [0.8000, -2.9000],
+                  [1.0000, -3.0000],
+                  [1.1000, -3.0000],
+                  [2.0000, -3.0000],
+                  [4.0000, -3.0000],
+                  [8.0000, -3.0000],
+                  [10.0000, -3.0000],
+                  [15.0000, -3.0000]])
+
+    # Interpolate K values between 0.125 and 15 kHz
+    f = interp1d(x=K[:, 0], y=K[:, 1], kind="cubic", fill_value="extrapolate")
+    KdB = f(all_freqs)
+
+    # Calculate ERB at each freq
+    ERB = 24.7 * ((4.37 * all_freqs) + 1)
+    cr_erb = KdB + (10*np.log10(ERB))
+    TEN_No = -cr_erb
+
+    def indices(a, func):
+        return [i for (i, val) in enumerate(a) if func(val)]
+    index1kERB = indices(all_freqs, lambda x: 0.935 < x < 1.0681)  # cams for 1 kHz +- 0.5 Cams
+    total_level_dB = 10*np.log10(np.sum(10**(TEN_No/10)))
+    total_level_1k = 10*np.log10(np.sum(10**(TEN_No[index1kERB]/10)))
+    ratio_1k_dB = total_level_dB - total_level_1k
+
+    magnitude_TEN = np.mean(10**(TEN_No/10))
+    magnitude_ratio_dB = 10*np.log10(magnitude_TEN) + 3
+
+    fspecfilt = deepcopy(fspec)
+
+    fspecfilt[LPbin:HPbin] = fspecfilt[LPbin:HPbin] * 10**(TEN_No[LPbin:HPbin]/20)
+
+    noise = np.fft.ifft(fspecfilt)
+    noise = np.real(noise[0:dur_smp])
+
+    noise = noise * np.sqrt(2*fftpts) / (10**(magnitude_ratio_dB/20)) * 10**(ratio_1k_dB/20)
+
+    noise = noise * 10**(level/20)
+
+    return noise
